2e9d982bce7004c2d1314f3bca3111cb2c946655
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-14 09:25:46 +0100

    .

---
+++ src/operaciones/CalculadoraTest.java
@@ -0,0 +1,39 @@
+package operaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testCalculadora() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculadoraIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
